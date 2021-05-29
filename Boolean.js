d44bf1966c6cfd33a3eaa34c43ec1293f3791cd5
H12M54AM <74957928+H12M54AM@users.noreply.github.com>
2021-05-28 22:25:08 -0700

    learned some things
    For the Functions, I have learned some new things and made my own function or actually couple of functions. I have added functions.js to the html and added Boolean.js in there as well. Boolean.js is very small with no code in the function

---
+++ Boolean.js
@@ -0,0 +1,6 @@
+
+function indent() {
+    
+    return false;
+}
+
--- functions.js
+++ functions.js
@@ -14,3 +14,30 @@ console.log(answer) //thing
 
 console.log(square(2));
 
+function nextInLine(arr, item) {
+
+    arr.push(item);
+    return arr.shift();
+}
+
+let myArr = [1, 2, 3, 4, 5,];
+
+console.log("Before: " + JSON.stringify(myArr));
+console.log(nextInLine(myArr, 6));
+console.log("After: " + JSON.stringify(myArr));
+
+
+function userInputs() {
+    let answer = window.prompt("Whats your name?")
+    document.write("Cool. I'm " + answer + " too ");
+    return true;
+}
+
+function howOld() {
+    let age = window.prompt("How old are you?" + answer)
+    document.write(" I'm also " + age + ", we must be twins!");
+    return true;
+}
+
+console.log(userInputs());
+console.log(howOld());
